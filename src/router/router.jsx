38aba2855479757e48dd08821174bd50eeaa0dc4
mariandrean <m.andreaan@gmail.com>
2024-05-18 22:57:53 +0200

    fix

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,25 +1,51 @@
-import { Outlet, createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import { getAllNews } from "../services/newsServices";
-import LayoutPublic from "../layout/LayoutPublic";
+import { createBrowserRouter } from 'react-router-dom';
+import LayoutPublic from '../layout/LayoutPublic.jsx';
+import Home from '../pages/Home.jsx';
+import Login from '../pages/Login.jsx';
+import NewsDetails from '../pages/NewsDetails.jsx'
+import LayoutPrivate from '../layout/LayoutPrivate.jsx';
+import Register from '../pages/Register.jsx';
+import Dashboard from '../pages/Dashboard.jsx';
+import { getAllNews, getNewsById } from '../services/newsServices.js';
+import ErrorBoundary from '../components/ErrorBoundary.jsx';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LayoutPublic />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-        loader: getAllNews
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      }
-    ]
-  }  
+    {
+        path: '/',
+        element: <LayoutPublic />,
+        errorElement: <ErrorBoundary />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+                loader: getAllNews
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/news/:id",
+                element: <NewsDetails />,
+                loader: ({params}) => getNewsById(params.id)
+            },
+        ],
+    },
+    {
+        path: "/dashboard",
+        element: <LayoutPrivate />,
+        children: [
+            {
+                index: true,
+                element: <Dashboard />,
+                loader: getAllNews
+            },
+            {
+                path: "/dashboard/register",
+                element: <Register />
+            }
+        ]
+    }
 ]);
 
 export default router;
